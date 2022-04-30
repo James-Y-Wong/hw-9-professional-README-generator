a94ffd97db0f72ce63198332a1fa708a33a35954
James-Y-Wong <jamesyeuwong@gmail.com>
2022-04-29 23:11:36 -0400

    finshed adding user prompt questions needed to genereate README

--- Develop/index.js
+++ Develop/index.js
@@ -36,8 +36,48 @@ const questions = [
             message: 'What are the instructions to run this program?',
             name: 'usage',
         },
-
-        
+        {
+            type: 'list',
+            message: 'What license do you want to use?',
+            choices: ['MIT', 'Apache 2.0', 'BSD 3-Clause', 'BSD 2=Clause', 'GPLv3', 'None'],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'What is the copyright year?',
+            name: 'year',
+        },
+        {
+            type: 'input',
+            message: 'Who is the copyright owner?',
+            name: 'owner',
+        },
+        {
+            type: 'input',
+            message: 'What is the repository name?',
+            name: 'contributing',
+        },
+        {
+            type: 'list',
+            message: 'Is there a test written for this application?',
+            choices: ['Yes', 'No'],
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'What is the link to the test?',
+            name: 'link',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
     ])
 ];
 
